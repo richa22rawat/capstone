585e04744ba23c165628e9650087df97342801b2
Rishabh Rawat <rishabhricharawat@gmail.com>
2022-07-22 16:45:29 +0530

    committed

---
+++ src/main/java/com/project/bicycleRentalApp/BicycleRentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.bicycleRentalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleRentalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleRentalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bicycleRentalApp/BicycleRentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bicycleRentalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleRentalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
